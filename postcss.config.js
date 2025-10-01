2e967d19057e049008bf23af1619310a263bea59
Victor Akande <victorakande847@gmail.com>
2025-10-01 19:00:51 +0200

    a

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,8 @@
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-import postcssImport from 'postcss-import'
-import postcssNesting from 'postcss-nesting'
-
 export default {
-  plugins: [
-    postcssImport,
-    postcssNesting,
-    tailwindcss,
-    autoprefixer,
-  ]
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    'tailwindcss': {},
+    'autoprefixer': {},
+  }
 }
