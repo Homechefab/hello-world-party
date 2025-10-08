040876a27aa43e93688c7e24cc07a10b4253daa6
Victor Akande <victorakande847@gmail.com>
2025-10-08 20:27:19 +0200

    a

--- supabase/functions/_shared/stripe.ts
+++ supabase/functions/_shared/stripe.ts
@@ -1,13 +1,13 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-import Stripe from 'https://esm.sh/stripe@14.14.0';
+import { createClient } from "@supabase/supabase-js";
+import Stripe from "stripe";
 
-// Initialize Stripe
+// Initialize Stripe with Deno env
 export const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') ?? '', {
   apiVersion: '2023-10-16',
   httpClient: Stripe.createFetchHttpClient(),
 });
 
-// Initialize Supabase client
+// Initialize Supabase client with Deno env
 export const supabaseClient = createClient(
   Deno.env.get('SUPABASE_URL') ?? '',
   Deno.env.get('SUPABASE_ANON_KEY') ?? '',
